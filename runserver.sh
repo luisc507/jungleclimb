9d68d1cc2c2ea2e1beff36c7989ef2262b2b1b85
jungleclimb <carranza_1294@hotmail.com>
2021-11-24 17:58:23 -0500

    change runserver

--- runserver.sh
+++ runserver.sh
@@ -2,4 +2,4 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-gunicorn --worker-tmp-dir /dev/shm junglewebsite.wsgi
+gunicorn --worker-tmp-dir /dev/shm junglewebsite.wsgi:application
